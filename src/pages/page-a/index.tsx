4375781205413279778c65e3fd9d41e69450ad55
yingjieweb <yingjieweb@qq.com>
2024-12-04 18:06:43 +0800

    feat: 🎸 Implement masonry infinite scroll comp

--- src/pages/page-a/index.tsx
+++ src/pages/page-a/index.tsx
@@ -1,14 +1,71 @@
-import React from 'react'
-import styles from './styles.module.scss'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import styles from "./styles.module.scss";
+import { Link } from "react-router-dom";
+import Masonry from "react-masonry-css";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-function PageA() {
+const initialProjects = [
+  { logo: "1", title: "Project 1", height: 40 },
+  { logo: "2", title: "Project 2", height: 80 },
+  { logo: "3", title: "Project 3", height: 120 },
+  { logo: "4", title: "Project 4", height: 160 },
+  { logo: "5", title: "Project 5", height: 200 },
+  { logo: "6", title: "Project 6", height: 240 },
+  { logo: "7", title: "Project 7", height: 280 },
+  { logo: "8", title: "Project 8", height: 320 },
+  { logo: "9", title: "Project 9", height: 360 },
+  { logo: "10", title: "Project 10", height: 320 },
+  { logo: "11", title: "Project 11", height: 280 },
+  { logo: "12", title: "Project 12", height: 240 },
+  { logo: "13", title: "Project 13", height: 200 },
+  { logo: "14", title: "Project 14", height: 160 },
+  { logo: "15", title: "Project 15", height: 120 },
+  { logo: "16", title: "Project 16", height: 80 },
+  { logo: "17", title: "Project 17", height: 40 },
+];
+
+const PageA: React.FC = () => {
+  const [projects, setProjects] = useState(initialProjects);
+
+  const onLoadMore = () => {
+    console.log("onLoadMore");
+    setProjects((prevProjects) => [...prevProjects, ...initialProjects]);
+  };
+
   return (
     <div className={styles.pageA}>
-      <div className="title">PageA</div>
+      <div className={styles.title}>PageA</div>
       <Link to="/page-b">PageB</Link>
-    </div>
-  )
-}
 
-export default PageA
+      <div className={styles.projectListContainer} id="scrollableDiv">
+        <InfiniteScroll
+          dataLength={projects.length}
+          next={onLoadMore}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+          endMessage={<h4>No more data</h4>}
+          scrollThreshold={0.9}
+          scrollableTarget="scrollableDiv"
+        >
+          <Masonry
+            className={styles.projectList}
+            columnClassName={styles.projectListColumn}
+            breakpointCols={{ default: 5, 992: 4, 786: 3 }}
+          >
+            {projects.map((project, index) => (
+              <div
+                className={styles.projectItem}
+                key={`${project.logo}-${index}`}
+                style={{ height: project.height }}
+              >
+                {project.title}
+              </div>
+            ))}
+          </Masonry>
+        </InfiniteScroll>
+      </div>
+    </div>
+  );
+};
+
+export default PageA;
